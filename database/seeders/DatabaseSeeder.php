dba387480cb11ae67395f46b7edc1240df5f0794
mieayambangka <frdeny12@gmail.com>
2024-12-05 15:38:28 +0700

    feat: initialize config folder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'role' => '1',
+            'status' => 1,
+            'hp' => '0812345678901',
+            'password' => bcrypt('P@55word'),
+        ]);
+        User::create([
+            'nama' => 'Sopian Aji',
+            'email' => 'sopian4ji@gmail.com',
+            'role' => '0',
+            'status' => 1,
+            'hp' => '081234567892',
+            'password' => bcrypt('P@55word'),
+        ]);
+        User::create([
+            'nama' => 'saipul jamil',
+            'email' => 'saipuljamil@gmail.com',
+            'role' => '2',
+            'status' => 0,
+            'hp' => '08123445677',
+            'password' => bcrypt('massaipul'),
+        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ resources/views/v_html/getlorem.blade.php
@@ -0,0 +1,12 @@
+<p>
+    <b>Mengenal Huruf Tebal</b><br><br>Lorem ipsum dolor, sit amet consectetur
+    adipisicing elit. Nulla est fugit porro possimus at iusto dolores debitis laborum
+    rerum, cum optio ipsam! Nesciunt doloremque tempore quibusdam fugit velit animi
+    suscipit.
+</p>
+<p>
+    <u>Mengenal Huruf Garis Bawah</u><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ut voluptatem totam aliquam libero. Facere, rem! Dolore autem praesentium qui aliquid quisquam recusandae quod repellendus, cupiditate ratione et magnam culpa.
+</p>
+<p>
+    <i> Mengenal Huruf Miring</i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, obcaecati, asperiores necessitatibus nam voluptate eius officia illum assumenda voluptatum omnis a deserunt cumque delectus consequatur reiciendis tempora officiis debitis aliquid.
+</p>
